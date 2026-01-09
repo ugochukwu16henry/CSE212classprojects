2378810d02bbc79976cffb329551832f56af97c7
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2026-01-09 18:14:38 +0100

    update

--- week01/teach/Program.cs
+++ week01/teach/Program.cs
@@ -5,13 +5,13 @@ Console.WriteLine("\n======================\nArray Selector\n===================
 ArraySelector.Run();
 
 // Uncomment when you are ready to run this
-// Console.WriteLine("\n======================\nAlgorithms\n======================");
+ //Console.WriteLine("\n======================\nAlgorithms\n======================");
 // Algorithms.Run();
 
 
 
 // Uncomment and run as you get to the solution part
-// Console.WriteLine("\n======================\nDivisors Solution\n======================");
+Console.WriteLine("\n======================\nDivisors Solution\n======================");
 // DivisorsSolution.Run();
 //
 // Console.WriteLine("\n======================\nArray Selector Solution\n======================");
